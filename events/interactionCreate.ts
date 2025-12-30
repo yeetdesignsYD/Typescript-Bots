78e6917c9a03b1093d743e93eacefe2ca1e767cf
DAREALYTYGRUNN1NGARK456 <65876060+DAREALYTYGRUNN1NGARK456@users.noreply.github.com>
2025-12-30 17:58:18 -0500

    Update interactionCreate.ts

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,5 +1,15 @@
 import { Client, Interaction } from "discord.js"
 
+import interactions = from '../utils/Interactions/index'
+
 export default async (client: Client, interaction: Interaction) => {
+  if (i.isChatInputCommand()) {
+            interactions.Commands(client, interaction)
+  } else if (i.isButton()) { 
+        interactions.Buttons(client, interaction)
+  }  else if (i.isStringSelectMenu()) {
+			// respond to the select menu
+            interactions.SelectMenus(client, interaction)
+		}
 
 }
