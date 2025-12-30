62e5e411edd4dae96dd51495c7972be330dba8fb
DAREALYTYGRUNN1NGARK456 <65876060+DAREALYTYGRUNN1NGARK456@users.noreply.github.com>
2025-12-30 14:04:38 -0500

    Update Commands.ts

--- utils/Interaction/Commands.ts
+++ utils/Interaction/Commands.ts
@@ -1,5 +1,28 @@
-import { Client, Interaction, EmbedBuilder } from 'discord.js'
+import { Client, Interaction, EmbedBuilder, MessageFlags } from 'discord.js'
 
 export default async (client: Client, interaction: Interaction) => {
 
+  const command = await client.commands.Slash.get(interaction.commandName)
+  if (!command) {
+    const NotFound = new EmbedBuilder()
+    .setTitle(`**${errorEmoji} Command not found ${errorEmoji}!**`)
+    .setColor("Red")
+
+    return interaction.reply({
+      embeds: [
+        NotFound
+        ],
+      flags: MessageFlags.Ephemeral
+    })
+
 }
+
+  try {
+    await command.execute ? command.execute(client, interaction) : command.run(client, interaction)
+  } catch (err) {
+    console.error(err)
+
+  }
+}
+  
+  
