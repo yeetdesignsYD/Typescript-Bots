0f3ae8033a3e1d7ecd0bacd4ed48bacdb18d092c
DAREALYTYGRUNN1NGARK456 <65876060+DAREALYTYGRUNN1NGARK456@users.noreply.github.com>
2025-12-30 13:14:22 -0500

    Update Phone.ts

--- commands/economy/Phone.ts
+++ commands/economy/Phone.ts
@@ -1,5 +1,5 @@
 import { Command } from '../../types/Command.ts'
-import { Client, Message, EmbedBuilder } from 'discord.js'
+import { Client, Message, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } from 'discord.js'
 
 
 const command: Command = {
@@ -9,50 +9,122 @@ const command: Command = {
   category: "economy",
   async run(client: Client, message: Message, args: string[]) {
 
-    const { guild } = message
-
-    const userID = args[0]
-
-    const member = await message.mentions.members.first()?.user || await guild.members.fetch(userID)?.user || await guild.members.cache.get(userID)?.user || message.member?.user
-
-    const Emojis = client.emojis
-
-    const Prefix = await client.db.fetch(`guild_${guild.id}`, "Prefix")
-
-     const guildEco = eco.guilds.get(message.guild.id)
-
-  const userEco = await guild.users.get(
-    member.id
-)
-
-  const [wallet, bank] = [
-      user.balance.get(),
-      user.bank.get()
-  ]
-
-    const BalEmbed = new EmbedBuilder()
-    .setTitle(`**${Emojis.Economy.BalanceCmd.Title} ${member.user.tag}'s Balance ${Emojis.Economy.BalanceCmd.Title}**`)
-    .setDescription(`
-    To check your YDCash or YDPay balance you must use ${Prefix}phone and select the corresponding app
-    `)
-    .addFields(
-      {
-        name: "**Wallet:**",
-        value: `$${wallet}`
-      },
-      {
-        name: "**${Emojis.Economy.Bank}Bank:**",
-        value: `$${bank}`
-      }
-      )
+    const PhoneEmbed = new EmbedBuilder()
+    .setImage("https://yeetdesigns.cc/Logo.png")
     .setColor("Blurple")
 
+    const emojis = {
+    ydCash: client.emojis.Economy.Phone.Apps.YDCash,
+    ydPay: client.emojis.Economy.Phone.Apps.YDPay,
+    bank: client.emojis.Economy.Phone.Apps.Bank,
+    store: client.emojis.Economy.Phone.Apps.Store
+    }
+
+    var phoneApps = new StringSelectMenuBuilder()
+			.setCustomId('phoneapps')
+			.setPlaceholder('Open an App!')
+
+    if (!emojis.ydCash && !emojis.ydPay && !emojis.bank && !emojis.store) {
+    phoneApps.addOptions(
+           new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('YDCash')
+					// Description of option
+					.setDescription('Open the YDCash app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_ydcash'),
+          new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('YDPay')
+					// Description of option
+					.setDescription('Open the YDPay app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_ydpay'),
+          new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('Bank')
+					// Description of option
+					.setDescription('Open the bank app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_bank'),
+          new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('Store')
+					// Description of option
+					.setDescription('Open the store app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_store')
+      )
+    } else if (emojis.ydCash && !emojis.ydPay && !emojis.store && !emojis.bank) {
+      
+    } else if (emojis.ydPay && !emojis.ydCash && !emojis.store && !emojis.bank) {
+      
+    } else if (emojis.bank && !emojis.store && !emojis.ydCash && !emojis.ydPay) {
+      
+    } else if (emojis.store && !emojis.bank && !emojis.ydCash && !emojis.ydPay) {
+      
+    } else if (emojis.ydCash && emojis.ydPay && !emojis.store && !emojis.bank) {
+    
+    } else if (emojis.bank && emojis.store && !emojis.ydCash && !emojis.ydPay) {
+    
+    } else if (emojis.ydCash && emojis.bank && !emojis.store && !emojis.ydPay) {
+      
+    } else if (emojis.ydCash && emojis.store && !emojis.ydPay && !emojis.bank) { 
+    
+    } else if (emojis.ydPay && emojis.bank && !emojis.store && !emojis.ydCash) {
+      
+    } else if (emojis.ydPay && emojis.store && !emojis.bank && !emojis.yCash) {
+      
+    } else {
+      phoneApps.addOptions(
+        new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('YDCash')
+          .setEmoji(emojis.ydCash)
+					// Description of option
+					.setDescription('Open the YDCash app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_ydcash'),
+        new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('YDPay')
+          .setEmoji(emojis.ydPay)
+					// Description of option
+					.setDescription('Open the YDPay app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_ydpay'),
+        new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('Bank')
+          .setEmoji(emojis.bank)
+					// Description of option
+					.setDescription('Open the Bank app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_bank'),
+        new StringSelectMenuOptionBuilder()
+					// Label displayed to user
+					.setLabel('Store')
+          .setEmoji(emojis.store)
+					// Description of option
+					.setDescription('Open the Store app')
+					// Value returned in select menu interaction
+					.setValue('phoneapp_store')
+    }
+
+    
+
+    
+
     message.reply({
-      embeds: [
-        BalEmbed
-        ]
+         embeds: [
+           PhoneEmbed
+           ],
+          components: [
+            ActionRow
+            ]
     })
     
+     
 
 
     
