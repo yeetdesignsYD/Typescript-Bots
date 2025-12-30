d3325bc577244ee0d3c8791aaedbc06f220d454d
DAREALYTYGRUNN1NGARK456 <65876060+DAREALYTYGRUNN1NGARK456@users.noreply.github.com>
2025-12-30 12:51:17 -0500

    Update Command.ts

--- types/Command.ts
+++ types/Command.ts
@@ -1,1 +1,20 @@
+import { Client, Interaction, Message, PermissionResolvable } from "discord.js"
 
+export interface CommandPrefix {
+name: string;
+description?: string;
+category?: string;
+aliases?: string[];
+permissions?: PermissionResolvable;
+run: (client: Client, message: Message, args: string[]) => Promise<void>;
+}
+export interface CommandSlash {
+data: {
+name: string;
+description: string;
+};
+run: (client: Client, interaction: Interaction) => Promise<void>;
+}
+
+
+  
